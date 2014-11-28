5a0fc82d3817e8c2ca638b35298c37626d0f1660
hakan <hakansel05@gmail.com>
2014-11-28 14:12:59 +0200

    monitorservice.sh script added.

---
+++ Monitoring/monitorservice.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+tarih=`/bin/date "+%Y%m%d-%H%M%S"`
+ip=`/sbin/ifconfig re1 | grep 'inet ' | awk '{print $2}'`
+
+MONITORING_PATH=/home/monitoring
+MONITORING_SCRIPT=stats_func.sh
+MONITORING_FILE=monitor-$ip-$tarih.json
+
+cd $MONITORING_PATH
+
+./$MONITORING_SCRIPT > $MONITORING_PATH/$MONITORING_FILE
+
+#/usr/local/bin/curl -H "Content-Type: application/json" -X POST --data @$MONITORING_FILE --url http://20.0.0.15:139
+#/usr/local/bin/curl -H "Content-Type: application/json" -X POST --data @$MONITORING_FILE --url http://20.0.0.100:4141/Monitoring
+/usr/local/bin/curl -H "Content-Type: application/json" -X POST --data @$MONITORING_FILE --url http://httpbin.org/post 
+
+if [ $? -ne 0 ]
+then
+	logger  "This time it is not completed to monitoring $tarih."
+else
+	rm  $MONITORING_PATH/$MONITORING_FILE
+fi
