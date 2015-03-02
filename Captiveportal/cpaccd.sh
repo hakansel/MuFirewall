cd076a41586d823c06c4d78fe443f85ca8a932a9
hakansel05 <hakansel05@gmail.com>
2015-03-03 00:59:36 +0200

    Status in added. Some control mechanism added.

--- Captiveportal/cpaccd.sh
+++ Captiveportal/cpaccd.sh
@@ -1,28 +1,62 @@
 #!/bin/sh
-#  This file should be added to /usr/local/etc/rc.d
+# This file was automatically generated
+# by the pfSense service handler.
+
+service=cpaccd.sh
+launcher=/root/cpacc_launcher.sh
+pid_file=/root/cpacc.pid
 
 rc_start() {
-        if [ -z "`ps auxw | grep "cp_access.sh`" ];then
-                sh /root/cpacc_launcher.sh
+        if [ -z "`ps auxw | grep "cp_access.sh`" ]; then
+                sh $launcher
         fi
-
 }
 
 rc_stop() {
-        cpacc_pid=`cat /root/cpacc.pid`
+        cpacc_pid=`cat $pid_file`
         kill -9 $cpacc_pid 2> /dev/null
-        rm /root/cpacc.pid 2> /dev/null
+        rm $pid_file 2> /dev/null
 }
 
 case $1 in
         start)
-                rc_start
+                if [ -z "`cat $pid_file`" ]; then
+                        logger -s Starting $service
+                        sleep 1
+                        rc_start
+                else
+                        psr=`cat $pid_file`
+                        logger -s $service is already running with pid = $psr
+                fi
                 ;;
         stop)
-                rc_stop
+                if [ -n "`cat $pid_file`" ]; then
+                        logger -s Stopping $service
+                        sleep 1
+                        rc_stop
+                else
+                        logger -s There are no running $service service to stop!!
+                fi
                 ;;
         restart)
-                rc_stop
-                rc_start
+                if [ -f $pid_file ]; then
+                        logger -s Stopping $service
+                        sleep 1
+                        rc_stop
+                else
+                        logger -s There are no running $service service to stop!!
+                fi
+                logger -s Starting $service
+                sleep 1
+                rc_start        
                 ;;
+        status)
+                if ! [ -f $pid_file ]; then
+                        logger -s $service service is not running!!
+                else
+                        if [ -n "`cat $pid_file`"  ]; then
+                                pid=`cat $pid_file`
+                                logger -s $service service is running with pid = $pid
+                        fi
+                fi
 esac
